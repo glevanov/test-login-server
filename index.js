93e939e934eebf471114edd5c9f17906f5b7c8e2
Grigoriy Levanov <glevanov@gmail.com>
2019-08-05 23:19:57 +0300

    Rewrite from flask to express

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes');
+
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+routes(app);
+
+const server = app.listen(port, (error) => {
+    if (error) return console.log(`Error: ${error}`);
+
+    console.log(`Server listening on port ${server.address().port}`);
+});
---
+++ routes/routes.js
@@ -0,0 +1,34 @@
+const multer  = require('multer');
+const upload = multer();
+
+const login = (username, code) => {
+    if (username === 'bob' && code === 'marley') {
+        return {
+            traffic: 12,
+            minutes: 13
+        }
+    } else {
+        return {
+            cat: 'meow'
+        }
+    }
+};
+
+const router = app => {
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
+    app.get('/', (req, res) => {
+        res.send('It works');
+    });
+
+    app.post('/check', upload.none(), (req, res) => {
+        const { username, code } = req.body;
+        res.send(login(username, code));
+    });
+};
+
+module.exports = router;
